33250425ed1b9f17fb3164474d6c83f6b1c6d8e8
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-21 13:58:34 +0200

    chore: Add toggle functionalityand links to githubs

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  isOpen = false;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleSidebar(): void {
+    this.isOpen = !this.isOpen;
+  }
 }
