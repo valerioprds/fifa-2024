5fc2d1db99abe1f3c60d86d2687a61b5ebdd5ee0
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-22 17:08:40 +0200

    created spinner and error page

--- src/app/shared/pages/not-found/not-found.component.ts
+++ src/app/shared/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class NotFoundComponent {
 
+  loading = false;
+
+  constructor(private router: Router) {}
+
+  goHome() {
+    this.loading = true;
+    setTimeout(() => {
+      this.router.navigate(['/home']);
+    }, 2000); // 2 seconds delay
+  }
 }
