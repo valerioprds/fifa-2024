03c205b613b4de244a605b49140d4cd9f2b1e1fe
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-23 16:48:28 +0200

    added languages buttons

--- src/app/shared/components/lang-btn/lang-btn.component.ts
+++ src/app/shared/components/lang-btn/lang-btn.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-lang-btn',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './lang-btn.component.scss'
 })
 export class LangBtnComponent {
+  selectedLanguage = 'en';
 
+  constructor(private translateService: TranslateService) {
+    this.translateService.setDefaultLang('en');
+    this.translateService.use(this.selectedLanguage);
+  }
+
+  onLanguageChange() {
+    this.translateService.use(this.selectedLanguage);
+  }
 }
