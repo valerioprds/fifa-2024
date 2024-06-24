fd1e822d04e33d20ac785d3c3405e8f06f6208a5
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-24 18:12:18 +0200

    removed code that caused problem

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { Tooltip } from 'bootstrap';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-  ngOnInit(): void {
-    const tooltipTriggerList = [].slice.call(
-      document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    );
-    tooltipTriggerList.map((tooltipTriggerEl) => new Tooltip(tooltipTriggerEl));
-  }
-}
+export class SidebarComponent {}
