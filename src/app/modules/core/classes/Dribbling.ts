981e5fc6028b7f0c0225eb7a39ba01f0493a0346
gallianoms <ggallianoms@gmail.com>
2024-06-24 16:36:00 +0200

    add validations config on classes

--- src/app/modules/core/classes/Dribbling.ts
+++ src/app/modules/core/classes/Dribbling.ts
@@ -9,11 +9,32 @@ export class Dribbling implements IDribbling {
   composure: number;
 
   constructor(config: IDribbling) {
+    this.ValidateDribblingConfig(config);
     this.agility = config.agility;
     this.balance = config.balance;
     this.reactions = config.reactions;
     this.ballControl = config.ballControl;
     this.dribbling = config.dribbling;
     this.composure = config.composure;
+  }
+
+  private ValidateDribblingConfig(config: IDribbling): void {
+    const attributes = [
+      'agility',
+      'balance',
+      'reactions',
+      'ballControl',
+      'dribbling',
+      'composure',
+    ];
+
+    for (const attribute of attributes) {
+      if (
+        typeof config[attribute as keyof IDribbling] !== 'number' ||
+        config[attribute as keyof IDribbling] < 0
+      ) {
+        throw new Error(`Invalid value for ${attribute}`);
+      }
+    }
   }
 }
--- src/app/modules/core/classes/Shooting.ts
+++ src/app/modules/core/classes/Shooting.ts
@@ -9,11 +9,32 @@ export class Shooting implements IShooting {
   penalties: number;
 
   constructor(config: IShooting) {
+    this.ValidateShootingConfig(config);
     this.attPosition = config.attPosition;
     this.finishing = config.finishing;
     this.shotPower = config.shotPower;
     this.longShots = config.longShots;
     this.volleys = config.volleys;
     this.penalties = config.penalties;
+  }
+
+  private ValidateShootingConfig(config: IShooting): void {
+    const attributes = [
+      'attPosition',
+      'finishing',
+      'shotPower',
+      'longShots',
+      'volleys',
+      'penalties',
+    ];
+
+    for (const attribute of attributes) {
+      if (
+        typeof config[attribute as keyof IShooting] !== 'number' ||
+        config[attribute as keyof IShooting] < 0
+      ) {
+        throw new Error(`Invalid value for ${attribute}`);
+      }
+    }
   }
 }
