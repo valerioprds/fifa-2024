66211a8e98d620962038c70b83f62945dc9ec870
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-25 21:42:17 +0200

    updated readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,3 +4,4 @@ export const environment = {
     'U2FsdGVkX1/HCJzyTwfnk8m2GgRpQC41/KSOHI8N0TnjordYT253ycyFJtYn53Mr',
   secretKey: 'ValerioParadisoIsTheBestTeacherEver',
 };
+//U2FsdGVkX1+rtHCZjzhlf7p7SgL1vu6FNkOJNvrJO+QFfTtYt/mCpqzWsTrPFW2M
