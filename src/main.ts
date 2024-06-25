36c49569a037adfd719991b0c596e9cea4e27232
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-25 19:12:33 +0200

    chore: Update environment file for development mode

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment.prod';
+import { environment } from './environments/environment.development';
 
 if (environment.production) {
   window.console.log = () => {};
