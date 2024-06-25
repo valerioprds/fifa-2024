5d7ac824964ed14c30ab5f01f7e289842cd7eccc
ValerioParadiso <v.paradiso89@gmail.com>
2024-06-25 18:53:22 +0200

    feat: Add environment variable for production mode
    This commit adds an environment variable for production mode in the development environment file. This variable will be used to conditionally disable console logs in the main.ts file when the application is running in production mode.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment.development';
 
+if (environment.production) {
+  window.console.log = () => {};
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
